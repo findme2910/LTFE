5570da45bcb83f55f2b68a01d503ba6e6cd2b737
LeThanhTam7723 <21130526@st.hcmuaf.edu.vn>
2024-05-19 22:13:40 +0700

    first commit

---
+++ src/pages/Login/Login.tsx
@@ -0,0 +1,4 @@
+function Login(){
+   return<div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur, ex rem! Amet corporis dolore eius ex fugit incidunt, ipsa necessitatibus nesciunt nulla placeat provident recusandae repellendus rerum similique totam voluptate?</div>
+}
+export default Login
