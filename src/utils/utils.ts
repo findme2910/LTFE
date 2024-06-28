cf7d73e4096d373cb7e60bd4186c9e5d743949dd
LeThanhTam7723 <21130526@st.hcmuaf.edu.vn>
2024-06-28 15:52:16 +0700

    format pubDate

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,1 +1,19 @@
+export default function convertTimeFormat(inputTime:string) {
+    // Tạo một đối tượng Date từ chuỗi đầu vào
+    let date = new Date(inputTime);
+
+    // Tạo các mảng để lưu trữ các giá trị ngày, tháng và năm
+    let daysOfWeek = ['Chủ nhật', 'Thứ hai', 'Thứ ba', 'Thứ tư', 'Thứ năm', 'Thứ sáu', 'Thứ bảy'];
+    let day = date.getDate();
+    let month = date.getMonth() + 1;
+    let year = date.getFullYear();
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
+
+    // Định dạng lại ngày tháng năm theo định dạng yêu cầu
+    let formattedDate = `${daysOfWeek[date.getDay()]}, ${day.toString().padStart(2, '0')}/${month.toString().padStart(2, '0')}/${year}, ${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+
+    return formattedDate;
+}
 
