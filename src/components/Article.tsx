2cce5a6e1d2b88badf3d36e914a81e26d5565d49
linh2113 <21130091@st.hcmuaf.edu.vn>
2024-06-29 09:56:57 +0700

    update ui

--- src/components/Article.tsx
+++ src/components/Article.tsx
@@ -4,7 +4,6 @@ import cheerio from 'cheerio'
 import LoadingDetail from '@/components/LoadingDetail'
 import { Helmet } from 'react-helmet'
 import DOMPurify from 'dompurify'
-import { useLocation } from 'react-router-dom'
 import {
    FacebookShareButton,
    FacebookIcon,
@@ -15,14 +14,14 @@ import {
    EmailShareButton,
    EmailIcon,
    TelegramShareButton,
-   TelegramIcon,
-} from 'react-share';
+   TelegramIcon
+} from 'react-share'
 
 export const Article = ({ url }: { url: string }) => {
    const [contents, setContents] = useState<string>('')
    const [titleArticle, setTitleArticle] = useState<string>('')
    const [descArticle, setDescArticle] = useState<string>('')
-   const { pathname } = useLocation()
+   const pathname = window.location.href
    const [isReading, setIsReading] = useState<boolean>(false)
    const [voice, setVoice] = useState<SpeechSynthesisVoice | null>(null)
    const [utterance, setUtterance] = useState<SpeechSynthesisUtterance | null>(null)
@@ -62,7 +61,7 @@ export const Article = ({ url }: { url: string }) => {
    useEffect(() => {
       const getVoices = () => {
          const voices = window.speechSynthesis.getVoices()
-         const vietnameseVoice = voices.find(voice => voice.lang === 'vi-VN')
+         const vietnameseVoice = voices.find((voice) => voice.lang === 'vi-VN')
          setVoice(vietnameseVoice || null)
       }
 
@@ -87,7 +86,9 @@ export const Article = ({ url }: { url: string }) => {
                alert('Vietnamese voice is not available on this system.')
                return
             }
-            const newUtterance = new SpeechSynthesisUtterance(DOMPurify.sanitize(contents, { ALLOWED_TAGS: [], ALLOWED_ATTR: [] }))
+            const newUtterance = new SpeechSynthesisUtterance(
+               DOMPurify.sanitize(contents, { ALLOWED_TAGS: [], ALLOWED_ATTR: [] })
+            )
             newUtterance.lang = 'vi-VN'
             newUtterance.voice = voice
             newUtterance.onend = () => setIsReading(false)
@@ -123,15 +124,54 @@ export const Article = ({ url }: { url: string }) => {
                <TelegramIcon size={40} round />
             </TelegramShareButton>
          </div>
-         <button onClick={handleReadAloud} className="my-3 p-2 flex items-center gap-x-2 bg-primaryColor text-white rounded">
-            {isReading ? <><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-  <path strokeLinecap="round" strokeLinejoin="round" d="M17.25 9.75 19.5 12m0 0 2.25 2.25M19.5 12l2.25-2.25M19.5 12l-2.25 2.25m-10.5-6 4.72-4.72a.75.75 0 0 1 1.28.53v15.88a.75.75 0 0 1-1.28.53l-4.72-4.72H4.51c-.88 0-1.704-.507-1.938-1.354A9.009 9.009 0 0 1 2.25 12c0-.83.112-1.633.322-2.396C2.806 8.756 3.63 8.25 4.51 8.25H6.75Z" />
-</svg>
-Tạm dừng</> : <><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-  <path strokeLinecap="round" strokeLinejoin="round" d="M19.114 5.636a9 9 0 0 1 0 12.728M16.463 8.288a5.25 5.25 0 0 1 0 7.424M6.75 8.25l4.72-4.72a.75.75 0 0 1 1.28.53v15.88a.75.75 0 0 1-1.28.53l-4.72-4.72H4.51c-.88 0-1.704-.507-1.938-1.354A9.009 9.009 0 0 1 2.25 12c0-.83.112-1.633.322-2.396C2.806 8.756 3.63 8.25 4.51 8.25H6.75Z" />
-</svg>
-Đọc tự động</>}
+         <button onClick={handleReadAloud} className='auto-read my-3 flex items-center justify-center gap-x-1'>
+            {isReading ? (
+               <>
+                  <svg
+                     xmlns='http://www.w3.org/2000/svg'
+                     fill='none'
+                     viewBox='0 0 24 24'
+                     strokeWidth={1.5}
+                     stroke='currentColor'
+                     className='size-6'
+                  >
+                     <path
+                        strokeLinecap='round'
+                        strokeLinejoin='round'
+                        d='M17.25 9.75 19.5 12m0 0 2.25 2.25M19.5 12l2.25-2.25M19.5 12l-2.25 2.25m-10.5-6 4.72-4.72a.75.75 0 0 1 1.28.53v15.88a.75.75 0 0 1-1.28.53l-4.72-4.72H4.51c-.88 0-1.704-.507-1.938-1.354A9.009 9.009 0 0 1 2.25 12c0-.83.112-1.633.322-2.396C2.806 8.756 3.63 8.25 4.51 8.25H6.75Z'
+                     />
+                  </svg>
+                  Tạm dừng
+               </>
+            ) : (
+               <>
+                  <svg
+                     xmlns='http://www.w3.org/2000/svg'
+                     fill='none'
+                     viewBox='0 0 24 24'
+                     strokeWidth={1.5}
+                     stroke='currentColor'
+                     className='size-6'
+                  >
+                     <path
+                        strokeLinecap='round'
+                        strokeLinejoin='round'
+                        d='M19.114 5.636a9 9 0 0 1 0 12.728M16.463 8.288a5.25 5.25 0 0 1 0 7.424M6.75 8.25l4.72-4.72a.75.75 0 0 1 1.28.53v15.88a.75.75 0 0 1-1.28.53l-4.72-4.72H4.51c-.88 0-1.704-.507-1.938-1.354A9.009 9.009 0 0 1 2.25 12c0-.83.112-1.633.322-2.396C2.806 8.756 3.63 8.25 4.51 8.25H6.75Z'
+                     />
+                  </svg>
+                  Đọc tự động
+               </>
+            )}
+            <div id='clip'>
+               <div id='leftTop' className='corner' />
+               <div id='rightBottom' className='corner' />
+               <div id='rightTop' className='corner' />
+               <div id='leftBottom' className='corner' />
+            </div>
+            <span id='rightArrow' className='arrow' />
+            <span id='leftArrow' className='arrow' />
          </button>
+
          <div
             dangerouslySetInnerHTML={{
                __html: DOMPurify.sanitize(contents)
