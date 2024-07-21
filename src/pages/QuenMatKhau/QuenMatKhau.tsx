0acfd231036bba9de19b49fc2565d72dcaae75d9
linh2113 <21130091@st.hcmuaf.edu.vn>
2024-07-21 08:55:09 +0700

    Sửa giao diện quên mật khẩu

--- src/pages/QuenMatKhau/QuenMatKhau.tsx
+++ src/pages/QuenMatKhau/QuenMatKhau.tsx
@@ -1,40 +1,49 @@
-import { getAuth, sendPasswordResetEmail } from "firebase/auth";
+import { getAuth, sendPasswordResetEmail } from 'firebase/auth'
 import { useNavigate } from 'react-router-dom'
-import React, { useState } from 'react';
+import React, { useState } from 'react'
+import { Input } from '@/components/ui/input'
+import { Button } from '@/components/ui/button'
 
 const ForgotPassword: React.FC = () => {
-   const navigate = useNavigate();
-   const [email, setEmail] = useState('');
+   const navigate = useNavigate()
+   const [email, setEmail] = useState('')
    const sendResetPasswordEmail = async (email: string) => {
-      const auth = getAuth();
+      const auth = getAuth()
       try {
-         await sendPasswordResetEmail(auth, email);
-         alert("Email đặt lại mật khẩu đã được gửi!");
-         navigate('/');
+         await sendPasswordResetEmail(auth, email)
+         alert('Email đặt lại mật khẩu đã được gửi!')
+         navigate('/')
       } catch (error) {
-         console.error("Error sending password reset email:", error);
-         alert("Đã xảy ra lỗi khi gửi email đặt lại mật khẩu.");
+         console.error('Error sending password reset email:', error)
+         alert('Đã xảy ra lỗi khi gửi email đặt lại mật khẩu.')
       }
-   };
-
+   }
 
    const handleSubmit = (e: React.FormEvent) => {
-      e.preventDefault();
-      sendResetPasswordEmail(email);
-   };
+      e.preventDefault()
+      sendResetPasswordEmail(email)
+   }
 
    return (
-      <div>
-         <h2>Quên mật khẩu</h2>
+      <div className='max-w-sm mx-auto'>
+         <h1 className='text-2xl font-bold text-center mb-4'>Quên mật khẩu</h1>
          <form onSubmit={handleSubmit}>
             <label>
                Email:
-               <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+               <Input
+                  type='email'
+                  placeholder='Nhập email của bạn'
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+               />
             </label>
-            <button type="submit">Gửi email đặt lại mật khẩu</button>
+            <Button type='submit' className='w-full mt-4'>
+               Gửi email đặt lại mật khẩu
+            </Button>
          </form>
       </div>
-   );
-};
+   )
+}
 
-export default ForgotPassword;
+export default ForgotPassword
